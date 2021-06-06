cae40205fc59170b2dd97baf01decc3d0d193c92
Chandralinjo <boris.majic@sbgenomics.com>
2021-06-06 23:33:59 +0200

    Finish scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -43,8 +43,22 @@ def get_repo_files(repo_url, limit=None, dir_limit=None, extension='.php'):
     useful_files = [file for file in files if file.endswith(extension)]
     processed_files = [file.replace('blob', 'raw') for file in useful_files]
 
-    if len(processed_files) > limit:
-        return processed_files[0:limit].copy()
+    if limit is not None:
+        if len(processed_files) > limit:
+            return processed_files[0:limit].copy()
+        else:
+            for directory in dirs:
+                curr_dir_url = 'https://github.com' + directory
+                files_from_dir = get_repo_files(
+                    repo_url=curr_dir_url, limit=dir_limit, dir_limit=dir_limit, extension=extension
+                )
+                processed_files = processed_files + files_from_dir
+                if len(processed_files) > limit:
+                    break
+        if len(processed_files) > limit:
+            return processed_files[0:limit].copy()
+        else:
+            return processed_files.copy()
     else:
         for directory in dirs:
             curr_dir_url = 'https://github.com' + directory
@@ -52,36 +66,42 @@ def get_repo_files(repo_url, limit=None, dir_limit=None, extension='.php'):
                 repo_url=curr_dir_url, limit=dir_limit, dir_limit=dir_limit, extension=extension
             )
             processed_files = processed_files + files_from_dir
-            if len(processed_files) > limit:
-                break
-    if len(processed_files) > limit:
-        return processed_files.copy()
+        return processed_files
 
 
-def create_php_dict(url, limit=5000, max_files_per_repo=50):
+def create_php_dict(url, limit=5000, max_files_per_repo=50, max_files_per_dir=10):
     """
     Generate a set of repo-file objects.
 
     :param url: Base url to github search page
     :param limit: Number of files to return. Files are returned as
     :param max_files_per_repo: How many files per repo to return? Point is to have a more diverse set of code blocks
+    :param max_files_per_dir: How many files per directory to return.
     :return: list of dicts. Each dict contains repo url and list of files from said repo
     """
 
     total_added_files = 0
     php_dict = {}
+    curr_url = url
 
     while total_added_files < limit:
         # Get repo list from the current page:
 
-        curr_page = urllib2.urlopen(url)
+        curr_page = urllib2.urlopen(curr_url)
         soup = BeautifulSoup(curr_page, 'html.parser')
         curr_page_repo_objects = soup.find_all("li", {"class": "repo-list-item"})
         curr_page_repos = [get_repo_url(repo) for repo in curr_page_repo_objects]
 
         # Collect php files from the repo
         for curr_repo in curr_page_repos:
-            curr_repo_files = get_repo_files(repo_url=curr_repo, limit=max_files_per_repo)
+            curr_repo_files = get_repo_files(repo_url=curr_repo, limit=max_files_per_repo, dir_limit=max_files_per_dir)
             total_added_files += len(curr_repo_files)
             php_dict[curr_repo] = curr_repo_files.copy()
+            print("Finished processing {}. Added total of {} files".format(curr_repo, len(curr_repo_files)))
+
+        # Go to next page
+        print("Went to the next page")
+        curr_url = 'https://github.com' + soup.find('a', {"class": "next_page"}).get('href')
+
+    return php_dict
 
