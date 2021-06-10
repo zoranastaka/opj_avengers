0cfc8370e24859052239c1fb6b344b2ecfd130f4
zoranaschtaka <zoranaschtaka@gmail.com>
2021-06-10 10:38:39 +0200

    - added comment parser for single and multiple line comments - export data to tab separated pair info doc as requested - export data to files (1 pair 1 file) as requested - export data to annotation matrix

---
+++ main.py
@@ -0,0 +1,6 @@
+from comment_parser.comment_parser import extract_comments, extract_comments_from_file
+
+
+extract_comments()
+
+# extract_comments_from_file('https://raw.githubusercontent.com/', '/PHPMailer/PHPMailer/raw/master/test/PHPMailerTest.php')
